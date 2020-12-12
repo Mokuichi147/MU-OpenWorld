e980a4e3b6c812dee774317f1fb4a01b2a6d137b
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-12-12 22:44:59 +0900

    [update] MacOSに対応した

--- Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
+++ Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
@@ -10,7 +10,7 @@ namespace GitHub.Unity
     {
         static UnityAPIWrapper()
         {
-#if UNITY_2018_2_OR_NEWER
+#if UNITY_2018_2_OR_NEWER && !UNITY_EDITOR_OSX && !UNITY_STANDALONE_OSX
             Editor.finishedDefaultHeaderGUI += editor => {
                 UnityShim.Raise_Editor_finishedDefaultHeaderGUI(editor);
             };
