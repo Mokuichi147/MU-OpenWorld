6ccfe3582333a996d39564ec1d4b54fe54723514
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2021-01-01 18:01:18 +0900

    [add] ブレハブをIDで呼び出すスクリプト

---
+++ Assets/MU-OpenWorld/Scripts/PrefabID.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenWorld
+{
+    public class PrefabID : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct PrefabData
+        {
+            public string ID;
+            public GameObject PrefabObject;
+        }
+
+        public PrefabData[] PrefabsInspector;
+        static public PrefabData[] Prefabs;
+        static private Dictionary<string, int> idDict;
+
+        public void Init()
+        {
+            Prefabs = PrefabsInspector;
+
+            idDict = new Dictionary<string, int>();
+            for (int i=0; i<Prefabs.Length; i++)
+            {
+                idDict.Add(Prefabs[i].ID, i);
+            }
+        }
+
+        static public GameObject GetPrefab(string id)
+        {
+            return Prefabs[idDict[id]].PrefabObject;
+        }
+    }
+}
