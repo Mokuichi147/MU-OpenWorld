b43331dc54351d6872f1bfa0f3c8986dcd2f071a
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2021-01-02 18:25:21 +0900

    [add] エディタ上にボタンを表示するスクリプト

---
+++ Assets/MU-OpenWorld/Scripts/ChunkObjectEditor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace OpenWorld
+{
+    [CustomEditor(typeof(ChunkObject))]
+    public class ChunkObjectEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            ChunkObject chunkObjectScript = target as ChunkObject;
+
+            if (GUILayout.Button("Chunk Add This GameObject"))
+            {
+                chunkObjectScript.Add();
+            }
+        }
+    }
+}
