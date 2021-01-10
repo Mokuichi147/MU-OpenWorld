b6df300bbebbe3d3fb47c147e29871901d432760
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2021-01-11 02:53:13 +0900

    [update] メニューからの画面遷移

--- Assets/MU-OpenWorld/Scripts/GameManager.cs
+++ Assets/MU-OpenWorld/Scripts/GameManager.cs
@@ -11,6 +11,10 @@ namespace OpenWorld
         public GameObject GameView;
         public GameObject MenuView;
         public GameObject LicenseView;
+        public GameObject AvatarView;
+
+        // カメラ関連
+        public GameObject PlayerCamera;
 
         // その他
         public PrefabID PrefabIDScript;
@@ -27,6 +31,7 @@ namespace OpenWorld
             GameView.SetActive(false);
             MenuView.SetActive(false);
             LicenseView.SetActive(false);
+            AvatarView.SetActive(false);
         }
 
         void Start()
@@ -61,6 +66,20 @@ namespace OpenWorld
         public void HideLicenseView()
         {
             LicenseView.SetActive(false);
+            MenuView.SetActive(true);
+        }
+
+        public void ShowAvatarView()
+        {
+            PlayerCamera.SetActive(false);
+            AvatarView.SetActive(true);
+            MenuView.SetActive(false);
+        }
+
+        public void HideAvatarView()
+        {
+            PlayerCamera.SetActive(true);
+            AvatarView.SetActive(false);
             MenuView.SetActive(true);
         }
 
